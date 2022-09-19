6ec36fd63d08db3b21c21098a6eec3e811cf2093
qzd1226 <690160142@qq.com>
2022-09-19 14:06:25 -0500

    JedisDemo1

---
+++ src/main/java/JedisDemo1.java
@@ -0,0 +1,43 @@
+import redis.clients.jedis.Jedis;
+import java.util.*;
+public class JedisDemo1 {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.1.89",6379);
+        String pong = jedis.ping();
+        System.out.println("连接成功："+pong);
+        //添加数据
+        jedis.set("k1","v1");
+        jedis.set("k2","v2");
+        jedis.set("k3","v3");
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys.size());
+        for(String key:keys){
+            System.out.println(key);
+        }
+        System.out.println(jedis.exists("k1"));
+        System.out.println(jedis.ttl("k1"));
+        System.out.println(jedis.get("k1"));
+
+
+        //williamlast@WilliamdeMBP ~ % sudo redis-server /usr/local/etc/redis.conf
+        //Password:
+        //williamlast@WilliamdeMBP ~ % redis-cli /usr/local/etc/redis.conf
+        //(error) ERR unknown command '/usr/local/etc/redis.conf', with args beginning with:
+        //williamlast@WilliamdeMBP ~ % redis-cli -p 6379
+        //127.0.0.1:6379> config get protected-mode
+        //1) "protected-mode"
+        //2) "yes"
+        //127.0.0.1:6379> config set protected-mode no
+        //OK
+        //127.0.0.1:6379> config get protected-mode
+        //1) "protected-mode"
+        //2) "no"
+        //127.0.0.1:6379> exit
+        //williamlast@WilliamdeMBP ~ % redis-cli -p 6379
+        //127.0.0.1:6379> config get protected-mode
+        //1) "protected-mode"
+        //2) "no"
+        //127.0.0.1:6379>
+        jedis.close();
+    }
+}
